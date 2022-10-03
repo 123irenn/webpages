8ce44a5fc1f2b3ccbf649773cdf104c9ee95dc74
123irenn <112928214+123irenn@users.noreply.github.com>
2022-10-03 12:05:48 +0530

    commit

---
+++ strinconCat.js
@@ -0,0 +1,4 @@
+console.log('Do you like Latte or '.concat('Cappuccino?'));
+const helloWorld = 'Hello World!';
+
+console.log(helloWorld.includes('World'));
--- string.js
+++ string.js
@@ -5,3 +5,6 @@ console.log(str.length);
 //uppercase
 console.log(str.toLowerCase());
 console.log(str.toUpperCase());
+const index=str.charAt(1);
+console.log("AtIndex:"+index);
+
