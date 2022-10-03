18af4279e3c264c95a980a0bb7b629f13b94d2f5
123irenn <112928214+123irenn@users.noreply.github.com>
2022-10-03 12:55:36 +0530

    Update strinconCat.js

--- strinconCat.js
+++ strinconCat.js
@@ -2,3 +2,15 @@ console.log('Do you like Latte or '.concat('Cappuccino?'));
 const helloWorld = 'Hello World!';
 
 console.log(helloWorld.includes('World'));
+const index=helloWorld.indexOf('W');
+console.log(index);
+const str=helloWorld.replace('Hello','Hi');
+console.log("--------After replacing------- ");
+console.log(str);
+console.log("------Searching--------");
+console.log(helloWorld.search(/hello/));
+console.log("--------------Slicing--------------");
+let fruits=["apple","orange","mango"];
+let slice=fruits.slice(5,15);
+console.log(slice);
+
