cba0d57cc19a4861569ae0a59a029fb92e98d3db
123irenn <112928214+123irenn@users.noreply.github.com>
2022-10-14 17:12:08 +0530

    commit

---
+++ todonew/src/app/app.component.ts
@@ -0,0 +1,49 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+
+  selector: 'app-root',
+
+  templateUrl: './app.component.html',
+
+  styleUrls: ['./app.component.css'],
+
+})
+
+export class AppComponent {
+
+  title = 'first_project';
+
+  list:any[]=[];
+  list2:any[]=[];
+
+ 
+
+  EnterSubmit(item:String,bo:any,bi:any)
+
+    {
+
+     
+
+      if(item!="")
+
+this.list.push({item});
+this.list2.push({bi});
+
+bo.value=""
+
+}
+
+  remove(i:number)
+
+  {
+
+this.list.splice(i,1);
+
+  }
+
+}
+
+
+
